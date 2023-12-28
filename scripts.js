a6f09264bf4980bbdfa1d3f8c502e66d8c67ce1a
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2023-12-28 20:52:19 +0100

    Build: Build getComputerChoice and playRound functions

---
+++ scripts.js
@@ -0,0 +1,42 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+
+    if (choice === 1) {
+        return "Rock";
+    } else if (choice === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    return getWinner(playerSelection, getComputerChoice);
+}
+
+function getWinner(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Tie";
+    } else if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            return "Loss";
+        } else {
+            return "Win";
+        }
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Scissors") {
+            return "Loss";
+        } else {
+            return "Win";
+        }
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            return "Loss";
+        } else {
+            return "Win";
+        }
+    } else {
+        return "Invalid Input";
+    }
+}
